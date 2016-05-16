593e3f7993e0398bdb0ac86c323cdf4aa6ea6d71
Danylo Markov <danylo.markov@gmail.com>
2016-05-16 23:20:52 +0300

    Add new elements, add toggle to generator

--- app/javascripts/elements.js
+++ app/javascripts/elements.js
@@ -35,6 +35,65 @@ export class AndElement extends Element {
   }
 }
 
+export class OrElement extends Element {
+  constructor(id) {
+    super(id);
+
+    // input
+    jsPlumb.addEndpoint(id, {
+      anchor: "Right",
+      isSource: true
+    }, common);
+    // outputs
+    jsPlumb.addEndpoint(id, {
+      anchor: [0, 0.75, -1, 0],
+      isTarget: true
+    }, common);
+    jsPlumb.addEndpoint(id, {
+      anchor: [0, 0.25, -1, 0],
+      isTarget: true
+    }, common);
+  }
+}
+
+export class XorElement extends Element {
+  constructor(id) {
+    super(id);
+
+    // input
+    jsPlumb.addEndpoint(id, {
+      anchor: "Right",
+      isSource: true
+    }, common);
+    // outputs
+    jsPlumb.addEndpoint(id, {
+      anchor: [0, 0.75, -1, 0],
+      isTarget: true
+    }, common);
+    jsPlumb.addEndpoint(id, {
+      anchor: [0, 0.25, -1, 0],
+      isTarget: true
+    }, common);
+  }
+}
+
+export class NotElement extends Element {
+  constructor(id) {
+    super(id);
+
+    // input
+    jsPlumb.addEndpoint(id, {
+      anchor: "Right",
+      isSource: true
+    }, common);
+    // outputs
+    jsPlumb.addEndpoint(id, {
+      anchor: "Left",
+      isTarget: true
+    }, common);
+  }
+}
+
 export class Generator extends Element {
   constructor(id) {
     super(id);
@@ -43,6 +102,9 @@ export class Generator extends Element {
       anchor: "Right",
       isSource: true
     }, common);
+
+    // toggle
+    $(`#${id}`).append('<label class="switch"><input type="checkbox"><div class="slider"></div></label>');
   }
 }
 
